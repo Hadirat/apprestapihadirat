07e5f730d7e4eef60f9f3dd0f1933e75636993b6
Hadirat <ndruruhadirat4@gmail.com>
2024-01-29 16:52:36 +0700

    Buat file controller

---
+++ controller.js
@@ -0,0 +1,92 @@
+'use strict';
+
+var response = require('./res');
+var connection = require('./koneksi');
+
+exports.index = function(req, res){
+    response.ok("Aplikasi API berjalan",res)
+};
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req, res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res)
+        }
+    });
+};
+
+//menampilkan data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req, res){
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+    function(error, rows, fileds){
+        if (error){
+            console.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
+};
+
+//menambahkan data mahasiswa
+exports.tambahmahasiswa = function(req, res){
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    Connection.query('INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?,?,?)',
+        [nim,nama,jurusan],
+        function (error, rows, fileds){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok("Berhasil menambahkan data!", res);
+            }
+        });
+};
+
+//mengubah data berdasarkan id
+exports.ubahmahasiswa = function(req, res){
+    var id = req.body.id_mahasiswa;
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('UPDATE mahasiswa SET nim=?, nama=?, jurusan=? WHERE id_mahasiswa=?', [nim,nama,jurusan],
+    function (error, rows, fileds){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok("Berhasil mengubah data", res);
+        }
+    });
+};
+
+//menghapus data mahasiswa
+exports.hapusmahasiswa = function(req, res){
+    var id = req.body.id_mahasiswa;
+    connection.query('DELETE FROM mahasiswa WHERE id_mahasiswa=?', [id],
+    function (error, rows, fileds){
+        if(error){
+            console.log(error);
+        }else{
+            response.ok("Berhasil Hapus data", res);
+        }
+    });
+};
+
+//menampilkan matakuliah group
+exports.tampilgroupmatakuliah = function(req, res){
+    connection.query('SELECT mahasiswa.id_mahasiswa, mahasiswa.nim, mahasiswa.nama, mahasiswa.jurusan, matakuliah.matakuliah, matakuliah.sks FROM krs JOIN matakuliah JOIN mahasiswa WHERE krs.id_matakuliah = matakuliah.id_matakuliah AND krs.id_mahasiswa = mahasiswa.id_mahasiswa ORDER BY mahasiswa.id_mahasiswa;',
+    function (error, rows, fileds){
+        if(error){
+            console.log(error);
+        }else{
+            response.oknested(rows, res);
+        }
+    }
+    )
+}
