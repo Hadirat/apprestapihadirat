aaa428d0728e8028d086f8a5cbfb1d5ba084dac6
Hadirat <ndruruhadirat4@gmail.com>
2024-01-29 16:54:57 +0700

    Membuat file routers

---
+++ routers.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { response } = require('express');
+
+module.exports = function(app){
+    var jsonku = require('./controller');
+
+    app.route('/')
+        .get(jsonku.index);
+    app.route('/tampil')
+        .get(jsonku.tampilsemuamahasiswa);
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid);
+    app.route('/tambah')
+        .post(jsonku.tambahmahasiswa);
+    app.route('/ubah')
+        .put(jsonku.ubahmahasiswa);
+    app.route('/hapus')
+        .delete(jsonku.hapusmahasiswa);
+    app.route('/tampilmatakuliah')
+        .get(jsonku.tampilgroupmatakuliah);
+}
